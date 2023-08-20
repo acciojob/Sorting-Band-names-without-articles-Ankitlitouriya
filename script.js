16247e1bc7139bf2c3c70a9c87d486c84c72e064
Ankit Litouriya <118179332+Ankitlitouriya@users.noreply.github.com>
2023-08-20 23:52:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,27 @@
-//your code here
+let bandNames = ['The Beatles', 'Led Zeppelin', 'Pink Floyd', 'Aerosmith', 'The Rolling Stones', 'Queen', 'Anathema'];
 
+// Function to remove articles and sort the band names
+function sortBandNames(bandNames) {
+    const articles = ['the', 'an', 'a'];
+    const sortedNames = bandNames.sort((a, b) => {
+        const cleanA = a.toLowerCase().replace(/^(the|an|a)\s+/i, '');
+        const cleanB = b.toLowerCase().replace(/^(the|an|a)\s+/i, '');
+        return cleanA.localeCompare(cleanB);
+    });
+    return sortedNames;
+}
+
+const sortedBandNames = sortBandNames(bandNames);
+
+// Generating the HTML list
+const ul = document.createElement('ul');
+ul.setAttribute('id', 'band');
+
+for (const name of sortedBandNames) {
+    const li = document.createElement('li');
+    li.textContent = name;
+    ul.appendChild(li);
+}
+console.log(sortedBandNames)
+// Adding the list to the HTML body
+document.body.appendChild(ul);
